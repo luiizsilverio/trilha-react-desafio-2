ea19c14a0e9aa6cde59b2afde4b353925a36b87f
luiizsilverio <luiiz.silverio@gmail.com>
2021-03-23 15:25:27 -0300

    falta resolver 5 testes

--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,7 +1,8 @@
 import { createContext, ReactNode, useContext, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
-import { Product, Stock } from '../types';
+import { Product } from '../types';
+//import { Product, Stock } from '../types';
 
 interface CartProviderProps {
   children: ReactNode;
@@ -23,28 +24,74 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
-
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
+    
+    if (!!storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const resp = await api.get(`stock/?id=${productId}`)
+      
+      if(!resp.data) {
+        toast.error('Erro na adição do produto')
+        return
+      }
+
+      const saldo = resp.data[0]
+      
+      if (saldo.amount <= 0) {        
+        toast.error('Quantidade solicitada fora de estoque')  
+        return      
+      }    
+    
+      let newCart = [...cart]      
+      const index = newCart.findIndex(prod => prod.id === productId)
+      
+      if (index >= 0) {
+        newCart[index].amount ++
+      } else {
+        const prods = await api.get(`products/?id=${productId}`)
+
+        if(!prods.data) {
+          toast.error('Erro na adição do produto')
+          return
+        }
+
+        const newProd = prods.data[0] 
+       
+        newCart.push({...newProd, amount: 1})
+      }
+
+      setCart(newCart)
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart));
+
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto')
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const index = cart.findIndex(prod => prod.id === productId)
+
+      if (index < 0) {
+        toast.error('Erro na remoção do produto')        
+        return
+      }
+
+      const newCart = cart.filter(prod => prod.id !== productId)
+
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart));
+
+      setCart([...newCart])
+
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto')
     }
   };
 
@@ -52,10 +99,41 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     productId,
     amount,
   }: UpdateProductAmount) => {
-    try {
-      // TODO
+    if (amount <= 0) {
+      toast.error('Quantidade inválida')
+      return
+    }
+        
+    try {     
+      const index = cart.findIndex(prod => prod.id === productId)
+      let qtAtual = 0
+
+      if (index >= 0) {
+        qtAtual = cart[index].amount
+      }
+    
+      if (amount > qtAtual) {
+        const resp = await api.get(`stock?id=${productId}`)
+        const saldo = resp.data[0].amount
+        
+        if (amount > saldo) {
+          toast.error('Quantidade solicitada fora de estoque')
+          return
+        }
+      }
+
+      const newCart = cart.map(prod => {
+        return prod.id === productId
+          ? {...prod, amount}
+          : {...prod}
+      })
+      
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(newCart));
+
+      setCart(newCart)  
+
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto')
     }
   };
 
--- src/services/api.ts
+++ src/services/api.ts
@@ -3,3 +3,8 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: 'http://localhost:3333',
 });
+
+/*
+Para rodar a fake API, abra um terminal e digite: yarn server.
+Em seguida, em outro terminal, digite: yarn start
+*/
